995e590873bdfafc107e8c6a9b9c6624289c2d56
Jinfei Fan <fanjinfei@hotmail.com>
2017-02-15 11:27:02 -0500

    release resource properly in refresh matplot events

--- scale.py
+++ scale.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import random
+import random, math, gc
 import sys
 from PyQt4 import QtGui
+from PyQt4.QtCore import QTimer
  
 from matplotlib.backends.backend_qt4agg import FigureCanvasQTAgg as FigureCanvas
 #from matplotlib.backends.backend_qt4agg import NavigationToolbar2QTAgg as NavigationToolbar
@@ -10,6 +11,15 @@ from matplotlib.backends.backend_qt4agg import NavigationToolbar2QT as Navigatio
 import matplotlib.pyplot as plt
 import scipy.spatial as spatial
 import numpy as np
+from collections import defaultdict
+
+
+def _get_data():
+    data= []
+    for i in range (0, 100):
+        p = ( round( random.uniform(1, 10), 4), round(random.uniform(2,20), 4) )
+        data.append(p)
+    return data
 
 def fmt(x, y):
     return 'x: {x:0.2f}\ny: {y:0.2f}'.format(x=x, y=y)
@@ -20,7 +30,7 @@ class FollowDotCursor(object):
     http://stackoverflow.com/a/13306887/190597 (unutbu)
     http://stackoverflow.com/a/15454427/190597 (unutbu)
     """
-    def __init__(self, ax, x, y, tolerance=5, formatter=fmt, offsets=(-20, 20), canvas = None):
+    def __init__(self, ax, x, y, tolerance=5, formatter=fmt, offsets=(-20, 20), canvas = None, plt=None):
         try:
             x = np.asarray(x, dtype='float')
         except (TypeError, ValueError):
@@ -43,10 +53,13 @@ class FollowDotCursor(object):
         self.dot = ax.scatter(
             [x.min()], [y.min()], s=130, color='green', alpha=0.7)
         self.annotation = self.setup_annotation()
-        plt.connect('motion_notify_event', self)
+        if plt:
+            plt.connect('motion_notify_event', self)
         if canvas:
-            canvas.mpl_connect('motion_notify_event', self)
+            self.cid = canvas.mpl_connect('motion_notify_event', self)
 
+    def __del__(self):
+        print ("cursor deleted")
     def scaled(self, points):
         points = np.asarray(points)
         return points * (self.scale, 1)
@@ -90,13 +103,121 @@ class FollowDotCursor(object):
             # IndexError: index out of bounds
             return self._points[0]
 
-def _get_data():
-    data= []
-    for i in range (0, 100):
-        p = ( random.uniform(1, 10), random.uniform(2,20) )
-        data.append(p)
-    return data
-    
+class Window(QtGui.QDialog):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+ 
+        self.figure = plt.figure()
+        self.ax = self.figure.add_subplot(111)
+        self.cursor = None
+        self.canvas = FigureCanvas(self.figure)
+ 
+         
+        self.toolbar = NavigationToolbar(self.canvas, self)
+        self.toolbar.hide()
+ 
+        # Just some button 
+        self.button = QtGui.QPushButton('Plot')
+        self.button.clicked.connect(self.plot)
+
+ 
+        self.button1 = QtGui.QPushButton('Zoom')
+        self.button1.clicked.connect(self.zoom)
+         
+        self.button2 = QtGui.QPushButton('Pan')
+        self.button2.clicked.connect(self.pan)
+         
+        self.button3 = QtGui.QPushButton('Home')
+        self.button3.clicked.connect(self.home)
+ 
+        self.button4 = QtGui.QPushButton('Scale')
+        self.button4.clicked.connect(self.scale)
+
+        self.button5 = QtGui.QPushButton('Exit')
+        self.button5.clicked.connect(self.equit)
+ 
+        # set the layout
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.toolbar)
+        layout.addWidget(self.canvas)
+        layout.addWidget(self.button)
+        layout.addWidget(self.button4)
+        layout.addWidget(self.button5)
+        layout.addWidget(self.button1)
+        layout.addWidget(self.button2)
+        layout.addWidget(self.button3)
+        self.setLayout(layout)
+        
+        self.data = _get_data()
+        self.mdata = []
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.tick)
+        #self.timer.start(1000)
+    def tick(self):
+        pass
+        #print 'tick'
+
+    def home(self):
+        self.toolbar.home()
+    def zoom(self):
+        self.toolbar.zoom()
+    def pan(self):
+        self.toolbar.pan()
+    def equit(self):
+        sys.exit(0)
+
+    def scale(self):
+        '''data = []
+        for (x,y) in self.data:
+            data.append( (x,y) )'''
+        #md = MData(self.data)
+        #self.data, self.mdata= md.getRecordSets()
+        self.plot()
+         
+    def plot(self):
+        ''' plot some random stuff 
+        data = [random.random() for i in range(25)]
+        ax = self.figure.add_subplot(111)
+        ax.hold(False)
+        ax.plot(data, '*-')
+        self.canvas.draw()'''
+        #self.ax.hold(False)
+        #y = [ v[0] for v in self.data]
+        #z = [ v[1] for v in self.data] #horizon
+        x,y = zip (*self.data)
+        if self.ax:
+            self.figure.delaxes( self.ax)
+            del self.ax
+            if self.cursor:
+                self.canvas.mpl_disconnect(self.cursor.cid)
+                del self.cursor
+            self.ax = self.figure.add_subplot(111)
+        #self.ax.clear()
+        gc.collect()
+        self.ax.scatter(x,y, s=4, color='blue', picker=True)
+
+        '''a,b = [1,2,3], [4,5,6]
+        c = zip(a,b)
+        d, e = zip( *c)
+        self.ax.scatter(d,e, s=3, color='red', picker=True)'''
+        m,n = (), ()
+        for ps in self.mdata:
+            d,e = zip (*ps)
+            m, n = m+d, n+e
+            self.ax.scatter(d,e, s=2, color='red')
+
+        self.cursor = FollowDotCursor(self.ax, x, y, tolerance=20, canvas=self.canvas)
+        self.canvas.draw()
+ 
+if __name__ == '__main__':
+    app = QtGui.QApplication(sys.argv)
+ 
+    main = Window()
+    main.setWindowTitle('Simple QTpy and MatplotLib example with Zoom/Pan')
+    main.show()
+ 
+    sys.exit(app.exec_())
+
 def mytest():
     data = _get_data()
 
@@ -144,89 +265,3 @@ def mytest():
     plt.grid(True)
     plt.show()
 
-
-class Window(QtGui.QDialog):
-    def __init__(self, parent=None):
-        super(Window, self).__init__(parent)
- 
-        self.figure = plt.figure()
-        self.ax = self.figure.add_subplot(111)
-        self.canvas = FigureCanvas(self.figure)
- 
-         
-        self.toolbar = NavigationToolbar(self.canvas, self)
-        self.toolbar.hide()
- 
-        # Just some button 
-        self.button = QtGui.QPushButton('Plot')
-        self.button.clicked.connect(self.plot)
-
- 
-        self.button1 = QtGui.QPushButton('Zoom')
-        self.button1.clicked.connect(self.zoom)
-         
-        self.button2 = QtGui.QPushButton('Pan')
-        self.button2.clicked.connect(self.pan)
-         
-        self.button3 = QtGui.QPushButton('Home')
-        self.button3.clicked.connect(self.home)
- 
-        self.button4 = QtGui.QPushButton('Scale')
-        self.button4.clicked.connect(self.scale)
-
-        self.button5 = QtGui.QPushButton('Exit')
-        self.button5.clicked.connect(self.equit)
- 
-        # set the layout
-        layout = QtGui.QVBoxLayout()
-        layout.addWidget(self.toolbar)
-        layout.addWidget(self.canvas)
-        layout.addWidget(self.button)
-        layout.addWidget(self.button4)
-        layout.addWidget(self.button5)
-        layout.addWidget(self.button1)
-        layout.addWidget(self.button2)
-        layout.addWidget(self.button3)
-        self.setLayout(layout)
-        
-        self.data = _get_data()
- 
-    def home(self):
-        self.toolbar.home()
-    def zoom(self):
-        self.toolbar.zoom()
-    def pan(self):
-        self.toolbar.pan()
-    def equit(self):
-        sys.exit(0)
-
-    def scale(self):
-        data = []
-        for (x,y) in self.data:
-            data.append( (x,y) )
-        self.data= _get_data()
-        self.plot()
-         
-    def plot(self):
-        ''' plot some random stuff 
-        data = [random.random() for i in range(25)]
-        ax = self.figure.add_subplot(111)
-        ax.hold(False)
-        ax.plot(data, '*-')
-        self.canvas.draw()'''
-        #self.ax.hold(False)
-        y = [ v[0] for v in self.data]
-        z = [ v[1] for v in self.data] #horizon
-        self.ax.clear()
-        self.ax.scatter(z,y, picker=True)
-        cursor = FollowDotCursor(self.ax, z, y, tolerance=20, canvas=self.canvas)
-        self.canvas.draw()
- 
-if __name__ == '__main__':
-    app = QtGui.QApplication(sys.argv)
- 
-    main = Window()
-    main.setWindowTitle('Simple QTpy and MatplotLib example with Zoom/Pan')
-    main.show()
- 
-    sys.exit(app.exec_())
