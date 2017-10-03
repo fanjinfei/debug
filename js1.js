28c4255e691bfebe0da221a9779657ec47422e4d
Jinfei Fan <fanjinfei@hotmail.com>
2017-10-03 09:44:15 -0400

    a simple nodejs server

--- js1.js
+++ js1.js
@@ -7,6 +7,54 @@ console.log('The result is displayed in the Command Line Interface');
 var http = require('http');
 var dt = require('./myfirstmodule');
 var url = require('url');
+var fs = require('fs'); 
+
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
+
+//var buf = Buffer.from('abc');
+//console.log(buf);
+
+/*fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+}); 
+
+fs.open('mynewfile2.txt', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+}); 
+
+fs.writeFile('mynewfile3.txt', 'Hello content!', function (err) {
+  if (err) throw err;
+  console.log('Saved!');
+}); 
+
+fs.unlink('mynewfile2.txt', function (err) {
+  if (err) throw err;
+  console.log('File deleted!');
+}); 
+
+fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
+  if (err) throw err;
+  console.log('File Renamed!');
+}); */
+
+    fs.readFile('demofile1.html', function(err, data) {
+        //res.write(data);
+	console.log(data);
+      });
 
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/plain'});
@@ -16,6 +64,7 @@ http.createServer(function (req, res) {
     var q = url.parse(req.url, true).query;
     var txt = q.year + " " + q.month +"\n";
     res.write(txt);
+
 
     res.end('\nHello World!');
 }).listen(8080); 
