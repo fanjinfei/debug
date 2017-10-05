49aa8d0b361b1b65e0c51dc4942852fb6fbc9795
Jinfei Fan <fanjinfei@hotmail.com>
2017-10-05 11:55:49 -0400

    default flask static page

---
+++ myFlaskServer.py
@@ -0,0 +1,24 @@
+# from http://flask.pocoo.org/ tutorial
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/<string:page_name>/')
+def static_page(page_name):
+    return "Hello World! [" + page_name + "]"
+    #return render_template('%s.html' % page_name)
+
+
+@app.route("/") # take note of this decorator syntax, it's a common pattern
+def hello():
+    return "Hello World!"
+
+@app.route("/app") # take note of this decorator syntax, it's a common pattern
+def app1():
+    return "Hello app World!"
+
+@app.route("/photo") # take note of this decorator syntax, it's a common pattern
+def photo():
+    return "Hello photo World!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
