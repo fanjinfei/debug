a0223cf2dd9ac1b94d4ab59836ecf343fbffee52
Jinfei Fan <fanjinfei@hotmail.com>
2017-12-14 09:26:53 -0500

    request bandwidth limit python

--- pmultiprocess.py
+++ pmultiprocess.py
@@ -3,7 +3,7 @@ import time
 
 num_procs = 2
 def do_work(i, message):
-  print i, "work",message ,"completed"
+  print (i, "work",message ,"completed")
 
 def worker(i, q):
   for item in iter( q.get, None ):
@@ -11,7 +11,7 @@ def worker(i, q):
     time.sleep(1)
     q.task_done()
 #  q.task_done()
-  print i, 'exit'
+  print (i, 'exit')
 
 q = multiprocessing.JoinableQueue()
 procs = []
@@ -22,7 +22,7 @@ for i in range(num_procs):
 for i in range(num_procs):
   procs[i].start()
 
-source = ['hi','there',None,'how','are','you','doing']
+source = ['hi','there','how','are','you','doing']
 for item in source:
   q.put(item)
 
@@ -33,5 +33,5 @@ for p in procs:
 for p in procs:
   p.join()
 
-print "Finished everything...."
-print "num active children:",multiprocessing.active_children()
+print ("Finished everything....")
+print ("num active children:",multiprocessing.active_children())
