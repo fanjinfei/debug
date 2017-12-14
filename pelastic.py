cf33f62c092f0f879e5ad9cf70ffd849e95191c6
Jinfei Fan <fanjinfei@hotmail.com>
2017-12-14 08:38:31 -0500

    multiprocess python

--- pelastic.py
+++ pelastic.py
@@ -19,8 +19,9 @@ class M_Eserver():
         self.es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
     def list_index(self):
         pass
-    def search(self, **kv): #value not anaylzed, can call 
+    def search(self, **kv): #value not anaylzed for prefix/term/wildcard, can call 
         # curl -XPOST 'localhost:9200/_analyze?analyzer=standard' -d'I love Bears and Fish.'
+        # 'text' can be used with 'analyzer':'lowercase_keyword' 
         print kv
         try:
             return self.es.search(**kv)
@@ -32,8 +33,8 @@ class M_Eserver():
 
     def test(self):
         es = self.es
-        es.index(index='test-index', doc_type='test', id=1, body={'test': 'test'})
-        es.delete(index='test-index', doc_type='test', id=1)
+        es.index(index='test-index', doc_type='test', id=1, body={"name": "Luke Skywalker", 'test': 'test'})
+        #es.delete(index='test-index', doc_type='test', id=1)
         es.index(index='sw', doc_type='people', id=1, body={
             "name": "Luke Skywalker",
             "height": "172",
@@ -49,9 +50,12 @@ class M_Eserver():
             print 'not found'
         print self.search(index="sw", body={"query": {"prefix" : { "name" : "Darth Vader" }}})
         print self.search(index="sw", body={"query": {"prefix" : { "name" : "Lu" }}})
-        kv = {'index':"sw", 'body':{"query": {"prefix" : { "name" : "lu" }}} }
+
+        #index: _all/empty, comma sepparated list of string
+        kv = {'index':'_all', 'body':{"query": {"prefix" : { "name" : "lu" }}} }
         print self.search(**kv)
         print self.search(index="sw", body={"query": {"match_all" : {  }}})
+
         q = self.search(index="sw", body={"query": {"fuzzy_like_this_field" : { "name" : {"like_text": "jaba", "max_query_terms":5}}}})
         print q
     
---
+++ pmultiprocess.py
@@ -0,0 +1,37 @@
+import multiprocessing
+import time
+
+num_procs = 2
+def do_work(i, message):
+  print i, "work",message ,"completed"
+
+def worker(i, q):
+  for item in iter( q.get, None ):
+    do_work(i, item)
+    time.sleep(1)
+    q.task_done()
+#  q.task_done()
+  print i, 'exit'
+
+q = multiprocessing.JoinableQueue()
+procs = []
+for i in range(num_procs):
+  procs.append( multiprocessing.Process(target=worker, args=(i, q)) )
+  procs[-1].daemon = True
+
+for i in range(num_procs):
+  procs[i].start()
+
+source = ['hi','there',None,'how','are','you','doing']
+for item in source:
+  q.put(item)
+
+q.join()
+for p in procs:
+  q.put( None )
+
+for p in procs:
+  p.join()
+
+print "Finished everything...."
+print "num active children:",multiprocessing.active_children()
