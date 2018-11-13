8688831b706c78bbde5bd8a5d7a4f428c94a5437
Jinfei Fan <fanjinfei@hotmail.com>
2018-11-13 08:38:37 -0500

    python logger

--- ph.py
+++ ph.py
@@ -15,18 +15,44 @@ from datetime import datetime
 import dateparser
 import logging
 from logging.handlers import RotatingFileHandler
+import httplib
+
+httplib.HTTPConnection.debuglevel = 1
 
 logger = logging.getLogger('peval.log')
 logger.setLevel(logging.DEBUG)
-handler = RotatingFileHandler('/home/esadmin/peval.log', maxBytes=200000, backupCount=10)
+handler = RotatingFileHandler('/home/esadmin/log/ph.log', maxBytes=200000, backupCount=10)
 logger.addHandler(handler)
+
+logger.setLevel(logging.DEBUG)
+req_log = logging.getLogger('requests.packages.urllib3')
+req_log.setLevel(logging.DEBUG)
+req_log.propagate = True
+req_log.addHandler(handler)
+
+class StreamToLogger(object):
+   """
+   Fake file-like stream object that redirects writes to a logger instance.
+   """
+   def __init__(self, logger, log_level=logging.INFO):
+      self.logger = logger
+      self.log_level = log_level
+      self.linebuf = ''
+
+   def write(self, buf):
+      for line in buf.rstrip().splitlines():
+         self.logger.log(self.log_level, line.rstrip())
+
+sl = StreamToLogger(logger, logging.INFO)
+sys.stdout = sl
+sys.stderr = sl
 
 
 def get_web_html(url):
     n = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     a = int(time.time())
     try:
-        res = requests.get(url=url, verify=False, timeout=100)
+        res = requests.get(url=url, verify=False, timeout=20)
         #raise Exception('except 1')
     except:
         s = ' '.join(['timeout ', '100' , 'from',  n, '--', url ])
@@ -45,17 +71,16 @@ def main():
     now = int(time.time())
     if now %3600 != 0:
        time.sleep(0.5)
-       continue
-    url = "https://www150.statcan.gc.ca/n1/en/dsbbcan"
+       #continue
+    url = "https://www.statcan.gc.ca/fra/concepts/industrie"
     get_web_html(url)
-    url = "https://www150.statcan.gc.ca/n1/en/media01"
-    get_web_html(url)
-    #raise Exception('except 2')
-    time.sleep(30)
+    break
+    time.sleep(3)
 
 while True:
     try:
         main()
+        break
     except:
         import traceback
         s = traceback.format_exc()
