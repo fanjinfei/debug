c7180d40be85b86ed87bd6d7800769fc3ebf870a
Jinfei Fan <fanjinfei@hotmail.com>
2019-12-06 13:52:16 -0500

    gtk3 signal_emit

--- gtk_example_1.c
+++ gtk_example_1.c
@@ -47,7 +47,39 @@ main_1 (int    argc,
 
   return status;
 }
+#if 0
+void
+viewer_file_write (ViewerFile   *self,
+                   const guint8 *buffer,
+                   gsize         size)
+{
+  g_return_if_fail (VIEWER_IS_FILE (self));
+  g_return_if_fail (buffer != NULL || size == 0);
 
+  /* First write data. */
+
+  /* Then, notify user of data written. */
+  g_signal_emit (self, file_signals[CHANGED], 0 /* details */);
+}
+void test() {
+    
+    file_signals[CHANGED] = 
+      g_signal_newv ("changed",
+                     G_TYPE_FROM_CLASS (object_class),
+                     G_SIGNAL_RUN_LAST | G_SIGNAL_NO_RECURSE | G_SIGNAL_NO_HOOKS,
+                     NULL /* closure */,
+                     NULL /* accumulator */,
+                     NULL /* accumulator data */,
+                     NULL /* C marshaller */,
+                     G_TYPE_NONE /* return_type */,
+                     0     /* n_params */,
+                     NULL  /* param_types */);
+                     
+    file = g_object_new (VIEWER_FILE_TYPE, NULL);
+    g_signal_connect (file, "changed", (GCallback) print_hello, NULL);
+    viewer_file_write (file, buffer, strlen (buffer));
+}
+#endif
 int main(int argc, char *argv[])
 {
     GtkWidget *window, *win_1;
