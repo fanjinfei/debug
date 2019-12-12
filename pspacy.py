b49e8d3dab4178198b3c02b164269b9c26034072
Jinfei Fan <fanjinfei@hotmail.com>
2019-12-12 09:36:12 -0500

    more spacy

--- pspacy.py
+++ pspacy.py
@@ -11,6 +11,27 @@ try:
 except:
     print('python -m spacy download en_core_web_sm')
     sys.exit(0)
+
+#vocab, word vector
+print(nlp.vocab['man'].vector)
+tokens = nlp(u'man vehicle school jfido')
+
+for token in tokens:
+    print(token.text, token.has_vector, token.vector_norm, token.is_oov)
+for token1 in tokens:
+    for token2 in tokens:
+        print(token1.text, token2.text, token1.similarity(token2))
+target = nlp("Cats are beautiful animals.")
+ 
+doc1 = nlp("Dogs are awesome.")
+doc2 = nlp("Some gorgeous creatures are felines.")
+doc3 = nlp("Dolphins are swimming mammals.")
+ 
+print(target.similarity(doc1))
+print(target.similarity(doc2))
+print(target.similarity(doc3))
+                
+#token
 doc = nlp("Apple is looking at buying U.K. startup for $1 billion")
 
 for token in doc:
@@ -38,6 +59,18 @@ def printree(root):
 
 [printree(sent.root) for sent in doc.sents]
 
+#chunking
+doc = nlp(u"First American Financial exposed 16 years worth of mortgage paperwork including bank accounts.")
+for chunk in doc.noun_chunks:
+    print(chunk.text, chunk.root.text, chunk.root.dep_, chunk.root.head.text)
+
+#named entity
+doc = nlp(u"I am planning to go to London in the morning at 10am, I have to buy a HP laptop and 2 speakers for less than 1000 dollars. I hope America and China tradewar won't affect prices.")
+for ent in doc.ents:
+  print(ent.text, ent.label_, ent.start_char, ent.end_char)
+#displacy.render(doc, style='ent', jupyter=True)
+displacy.render(doc, style='ent')
+
 train_data = [
     ("Who is Chaka Khan?", [(7, 17, "PERSON")]),
     ("I like London and Berlin.", [(7, 13, "LOC"), (18, 24, "LOC")]),
@@ -45,6 +78,7 @@ train_data = [
 
 doc = Doc(nlp.vocab, ["rats", "make", "good", "pets"])
 gold = GoldParse(doc, entities=["U-ANIMAL", "O", "O", "O"])
+
 
 '''
 >>> print(en.meta["labels"]["parser"])
